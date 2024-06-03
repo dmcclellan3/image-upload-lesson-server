620e3116dad8321d5bf704761039a372bd4ec3dd
dmcclellan3 <wdmcclellan3@gmail.com>
2024-06-03 14:50:53 -0400

    saving

--- project_image_upload/urls.py
+++ project_image_upload/urls.py
@@ -5,10 +5,19 @@ from rest_framework_simplejwt.views import (
   TokenRefreshView,
 )
 from app_image_upload.views import *
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-image/', create_image),
+    path('get-images/', get_images),
     path('profile/', get_profile),
     path('refresh/', TokenRefreshView.as_view()),
     path('token/', TokenObtainPairView.as_view()),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
